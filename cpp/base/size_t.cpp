39fa2ae2bbae6ca9df15cd13e239c7f18dbc02d7
lihaiming <lihaiming@bytedance.com>
2022-10-14 14:29:55 +0800

    确认 size_t 为无符号数

---
+++ cpp/base/size_t.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    size_t i = -100;
+    int c = -100;
+    cout << "i: " << i << ", c: " << c << endl;
+    return 0;
+}
