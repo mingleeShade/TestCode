32309fdf790cd1755b27767100aa08eaeb030f1b
lihaiming <lihaiming@bytedance.com>
2023-01-16 10:38:13 +0800

    CSAPP 练习4.4 -- 补充注释

--- cpp/CSAPP_深入理解计算机系统/exercises_4.4.c
+++ cpp/CSAPP_深入理解计算机系统/exercises_4.4.c
@@ -11,6 +11,8 @@ long long push_test()
      * subl %edx, %eax      # 0 or 4
      * movl %eax, rval      # Set as return value
      */
+    // 上述汇编是 32 位环境下，64 位环境下，pushl 和 popl 应该改为 pushq 和 popq，
+    // 因此干脆将所有指令和寄存器都改为 64 位的
     asm("movq %%rsp,%%rax;pushq %%rsp;popq %%rdx;subq %%rdx,%%rax;movq %%rax,%0"
         : "=r" (rval)
         : /* No Input */
