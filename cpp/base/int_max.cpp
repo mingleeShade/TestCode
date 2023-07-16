239c3c8eba9e2a770fe7a5e4381fa8d53e036f13
lihaiming <lihaiming@bytedance.com>
2023-07-16 19:37:55 +0800

    测试 INT_MAX 大小

---
+++ cpp/base/int_max.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    std::cout << INT_MAX << endl;
+    return 0;
+}
