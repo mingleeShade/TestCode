c6a1c77b20d7de31647eb677aed9331e5a96dfd4
lihaiming <lihaiming@bytedance.com>
2023-07-24 14:49:57 +0800

    测试 字符串转换

--- python/test_string_index.py
+++ python/test_string_index.py
@@ -1,9 +1,13 @@
 str = "12.12B"
 str = "12.12K"
+data = float(str[:-1])
 
 if str[-1] == "B":
     print("BBBl")
 elif str[-1] == "K":
     print("KKK")
+    data *= 1024
 else:
     print("MMM")
+    data *= 1024 * 1024
+print(int(data))
