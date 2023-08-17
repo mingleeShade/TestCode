2e775eae3a627a7550cbacb7dc726b2a1fd9850d
lihaiming <lihaiming@bytedance.com>
2023-08-17 10:22:42 +0800

    测试数组遍历效率

---
+++ cpp/performance/iter_per_10000.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+struct Temp
+{
+    int a;
+    int b;
+    int c;
+};
+
+class Value
+{
+public:
+
+    int v = 0;
+    std::string name;
+    std::vector<Temp> vec;
+};
+
+int main()
+{
+    ;
+    return 0;
+}
