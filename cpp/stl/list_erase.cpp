85df0c6d481ecf6af0b80b3cd80d0b6f2a6f43c9
lihaiming <lihaiming@bytedance.com>
2023-12-05 14:15:00 +0800

    测试 list erase 是否会出错

---
+++ cpp/stl/list_erase.cpp
@@ -0,0 +1,27 @@
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+void show(const list<int>& l)
+{
+    for (const auto& param : l)
+    {
+        cout << param << ",";
+    }
+    cout << endl;
+}
+
+int main (int argc, char* argv[])
+{
+    std::list<int> l = {1, 2, 3, 4, 5};
+    auto it = l.begin();
+    ++it;//2
+    auto it2 = it;
+    // ++it2;//3
+    l.erase(it2);
+    l.erase(it);// use after free,但是 这一行改成 ++it，不会出错
+    // cout << "*it should == 4:" << *it << endl;
+    show(l);
+    return 0;
+}
