d23fc4c79bd26c2e266a4c386dee09a28601c426
lihaiming <lihaiming@bytedance.com>
2024-01-19 11:29:57 +0800

    go相关

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module minglee/go_test
+
+go 1.18
---
+++ go/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main()  {
+    fmt.Println("Hello world")
+    a := 10
+    fmt.Printf("Type of a, %T\n", a)
+}
