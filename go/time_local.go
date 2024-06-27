e9d1388ded64b729f7dc77b07f0749473bdc67e3
lihaiming <lihaiming@bytedance.com>
2024-06-27 19:18:46 +0800

    test

--- go/time_local.go
+++ go/time_local.go
@@ -8,4 +8,11 @@ import (
 func main() {
 	loc := time.Local
 	fmt.Printf("TZ=%v\n", *loc)
+	var t = time.Now()
+	fmt.Printf("TZ=%v\n", *t.Location())
+
+	t2 := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, loc)
+	name, offset := t2.Zone()
+	fmt.Printf("t2: %v, zone name: %s, offset: %d\n", t2, name, offset)
+
 }
