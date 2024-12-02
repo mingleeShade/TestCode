c60bcfc966d3f579b2bb49c2e779de1300ac24dd
lihaiming <lihaiming@bytedance.com>
2024-12-02 12:48:49 +0800

    测试 defer

--- go/deffer_code.go
+++ go/deffer_code.go
@@ -9,6 +9,7 @@ func Test() {
 	}()
 	defer func() {
 		if a == 1 {
+			// defer 的 return 只影响本 func
 			fmt.Println("a == 1")
 			return
 		}
@@ -22,6 +23,22 @@ func Test() {
 	a = 1
 }
 
+func TestDeferTime() {
+	defer func() {
+		fmt.Println("first push")
+	}()
+
+	if 1 == 1 {
+		return
+	}
+
+	// 未 push 的 defer 不会执行
+	defer func() {
+		fmt.Println("after return")
+	}()
+}
+
 func main() {
 	Test()
+	TestDeferTime()
 }
